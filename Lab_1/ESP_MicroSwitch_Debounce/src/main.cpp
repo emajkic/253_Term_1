f09b93f0b118448ae8735bc0b777067e52929adf
Ella Majkic <emajkic@student.ubc.ca>
2025-05-22 16:33:59 -0700

    Non-Interrupt Switch Code (V1)

--- Lab_1/ESP_MicroSwitch_Debounce/src/main.cpp
+++ Lab_1/ESP_MicroSwitch_Debounce/src/main.cpp
@@ -1,7 +1,8 @@
 #include <Arduino.h>
 #include <Wire.h>
 #include <Adafruit_GFX.h>
-#include <Adafruit_SSD1306.h>#include "driver/gpio.h"
+#include <Adafruit_SSD1306.h>
+#include "driver/gpio.h"
 
 #define SCREEN_WIDTH 128 // OLED display width, in pixels
 #define SCREEN_HEIGHT 64 // OLED display height, in pixels
@@ -16,10 +17,13 @@ Adafruit_SSD1306 display_handler(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET)
 bool switchPressed;
 
 void OledSetup(){
+  display_handler.begin(SSD1306_SWITCHCAPVCC, 0x3C); // Displays Adafruit logo by default. call clearDisplay immediately if you don't want this.
   display_handler.display();
   delay(2000);
  
   display_handler.clearDisplay();
+
+
   display_handler.setTextSize(1);
   display_handler.setTextColor(SSD1306_WHITE);
   display_handler.setCursor(0,0);
@@ -36,9 +40,15 @@ void loop() {
   switchPressed = digitalRead(PIN_LIMIT_SWITCH);
 
   if (switchPressed) {
+    display_handler.setCursor(0,0);
+    display_handler.clearDisplay();
     display_handler.println("Switch pressed!");  
+    display_handler.display();
   } else {
+    display_handler.setCursor(0,0);
+    display_handler.clearDisplay();
     display_handler.println("Switch not pressed...");
+    display_handler.display();
   }
 
   delay(500);
